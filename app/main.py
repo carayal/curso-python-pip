592f72999e831d7a0832168df1a0ac76a23140f1
carayal <cristhian.araya.l@gmail.com>
2023-05-10 23:10:56 -0400

    modificaciones en main, se agrega gráfico de barras en base a pais ingresado por usuario

--- app/main.py
+++ app/main.py
@@ -11,6 +11,14 @@ def run():
   percentages=list(map(lambda x:x['World Population Percentage'],data))
   charts.generate_pie_chart(countries,percentages)
 
+  country= input('Type country: ')
+  result=utils.population_by_country(data,country)
+
+  if len(result)>0:
+    country=result[0]
+    labels,values=utils.get_population(country)
+    charts.generate_bar_chart(labels,values)
+
 
 if __name__ == '__main__':
   run()
